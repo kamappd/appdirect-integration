d23a54f708505a2f76617b7cf51d1905242ecd63
David CHAU <david.chau@motricity.com>
2015-04-02 22:36:21 -0400

    try to secure with oauth 2 legged auth

--- src/main/java/com/appdirect/integration/security/oauth/OAuthProcessingFilterEntryPointImpl.java
+++ src/main/java/com/appdirect/integration/security/oauth/OAuthProcessingFilterEntryPointImpl.java
@@ -10,18 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class OAuthProcessingFilterEntryPointImpl extends OAuthProcessingFilterEntryPoint {
-    public void commence( HttpServletRequest request, HttpServletResponse response, AuthenticationException failure )
-            throws IOException, ServletException
-    {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException failure)
+            throws IOException, ServletException {
         // TODO Add your custom error handling here.
         // Failures here will be Spring Security's.
         // Here are some of the Exceptions you can handle:
         // InsufficientAuthenticationException
 
 //        response.setCharacterEncoding( "UTF-8" );
-        response.setStatus( HttpServletResponse.SC_UNAUTHORIZED );
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-//        response.setContentType( MediaType.APPLICATION_JSON );
+        response.setContentType(MediaType.APPLICATION_XML_VALUE);
 //
 //        // This response is for simplicity. Don't build your JSON responses like this! =)
 //        response.getWriter().println( "{\"Unauthorized\":\"" + failure + "\"}" );
