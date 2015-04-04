d3412d57647e05ce26977f371778086fb407a38f
David CHAU <david.chau@motricity.com>
2015-04-04 14:09:14 -0400

    try to use websocket

--- src/main/java/com/appdirect/integration/controllers/EventsController.java
+++ src/main/java/com/appdirect/integration/controllers/EventsController.java
@@ -5,6 +5,7 @@ import com.appdirect.integration.models.ResponseMessage;
 import com.appdirect.integration.models.SuccessResponseMessage;
 import com.appdirect.integration.models.events.Event;
 import com.appdirect.integration.services.EventsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
@@ -14,6 +15,9 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
@@ -23,21 +27,27 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
-public class EventsController {
+public class EventsController extends TextWebSocketHandler {
 
     private EventsService eventsService;
-
+    private ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     public EventsController(EventsService eventsService) {
         this.eventsService = eventsService;
     }
 
-    @MessageMapping("/events")
-    @SendTo("/topic/events")
-//    @RequestMapping(value = "/events", method = GET, produces = APPLICATION_JSON_VALUE)
-//    @ResponseBody
+    @RequestMapping(value = "/events", method = GET, produces = APPLICATION_JSON_VALUE)
+    @ResponseBody
     public List<Event> getEvents() throws Exception {
         return eventsService.getEvents();
+    }
+
+    @Override
+    @MessageMapping("/events")
+    @SendTo("/topic/events")
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        session.sendMessage(new TextMessage(objectMapper.writeValueAsString(eventsService.getEvents())));
     }
 
     @RequestMapping(value = "/test/create", method = GET, produces = APPLICATION_JSON_VALUE)
