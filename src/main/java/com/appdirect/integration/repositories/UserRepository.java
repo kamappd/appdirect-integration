11f93e8f10caaadbc5e329587df5a568a277e654
David CHAU <david.chau@motricity.com>
2015-04-05 16:33:42 -0400

    adding assigment/unassignment

--- src/main/java/com/appdirect/integration/repositories/UserRepository.java
+++ src/main/java/com/appdirect/integration/repositories/UserRepository.java
@@ -1,16 +1,21 @@
 package com.appdirect.integration.repositories;
 
 import com.appdirect.integration.models.User;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
     UserDetails findByOpenId(String openId);
 
+    @Modifying
+    @Transactional
     @Query("delete from User u where u.openId=:openId")
     void deleteByOpenId(@Param("openId") String openId);
 }
