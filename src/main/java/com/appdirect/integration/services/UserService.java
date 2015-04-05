87db29a54495da4ebef66d438322f9c040c32087
David CHAU <david.chau@motricity.com>
2015-04-05 13:40:45 -0400

    try to add openId auth

--- src/main/java/com/appdirect/integration/services/UserService.java
+++ src/main/java/com/appdirect/integration/services/UserService.java
@@ -3,6 +3,8 @@ package com.appdirect.integration.services;
 import com.appdirect.integration.models.User;
 import com.appdirect.integration.repositories.UserRepository;
 import com.appdirect.integration.utils.IdGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +17,9 @@ import static com.google.common.collect.Lists.newArrayList;
 
 @Service
 public class UserService implements UserDetailsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     private UserRepository userRepository;
     private IdGenerator idGenerator;
 
@@ -36,6 +41,7 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Loading user with openId {}", username);
         return userRepository.findByOpenId(username);
     }
 }
